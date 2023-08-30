e7387ddb11f7c668d33da3b73f759422673027ca
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2023-08-30 21:19:27 +0300

    Added SQL Server integration & Unit tests

---
+++ Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Packt.Shared
+{
+    public static class NorthwindContextExtensions
+    {
+        public static IServiceCollection AddNorthwindContext(
+            this IServiceCollection services,
+            string connectionString = "Data Source=.;Initial Catalog=Northwind;Integrated Security=true;MultipleActiveResultsets=true;Encrypt=false")
+        {
+            services.AddDbContext<NorthwindContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+
+                options.LogTo(Console.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+            });
+
+            return services;
+        }
+    }
+}
