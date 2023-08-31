76ea927b3dc114b5e9b9b5ca1738774120cd9207
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2023-08-31 17:30:10 +0300

    Add Razor base ++

--- Northwind.Web/Pages/Suppliers.cshtml.cs
+++ Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,20 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
 
 namespace Northwind.Web.Pages
 {
     public class SuppliersModel : PageModel
     {
-        public IEnumerable<string>? Suppliers { get; set; }
+        public IEnumerable<Supplier>? Suppliers { get; set; }
+        [BindProperty]
+        public Supplier? Supplier { get; set; } 
+
+        private NorthwindContext db;
+
+        public SuppliersModel(NorthwindContext db)
+        {
+            this.db = db;
+        }
 
         public void OnGet()
         {
             ViewData["Title"] = "Northwind B2B - Suppliers";
 
-            Suppliers = new[]
+            Suppliers = db.Suppliers.OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
+        }
+
+        public IActionResult OnPost()
+        {
+            if ((Supplier is not null) && ModelState.IsValid)
             {
-                "Alpha Co", "Beta Limited", "Gamma Corp"
-            };
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
